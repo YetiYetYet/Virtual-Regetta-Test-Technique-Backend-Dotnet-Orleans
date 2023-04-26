41ce6cafc42f4e53ba09d7eae86f56f8ed1c4493
YetiYetYet <alexisbehier22@gmail.com>
2023-04-26 15:05:55 +0200

    Installation

---
+++ GameSilos/Program.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+await Host.CreateDefaultBuilder()
+    .UseOrleans(builder =>
+    {
+        builder.UseLocalhostClustering();
+        builder.AddMemoryGrainStorageAsDefault();
+    })
+    .ConfigureLogging(builder => builder.AddConsole())
+    .RunConsoleAsync();
